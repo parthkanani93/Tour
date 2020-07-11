87fadbd44fa13001150cb7edd98f6c22d1907c73
parth <parthkanani8490@gmail.com>
2020-07-11 04:52:19 -0700

    add seprate file of json data

---
+++ src/shared/list.js
@@ -0,0 +1,67 @@
+const LIST = [
+    {
+      id: 1,
+      name: "Tajmahal",
+      link: "https://www.youtube.com/embed/44rTTLjZZz4?start=10&end=476"
+    },
+    {
+      id: 2,
+      name: "Great Wall Chaina",
+      link: "https://www.youtube.com/embed/EotbKqZmBuY?start=19&end=294"
+
+    },
+    {
+      id: 3,
+      name: "Christ the Redeemer Statue",
+      link: "https://www.youtube.com/embed/hpoGtOfZoYY?start=0"
+    },
+    {
+      id: 4,
+      name: "Machu Picchu (Peru)",
+      link: "https://www.youtube.com/embed/lNIEZ61PyG0?start=6"
+    },
+    {
+      id: 5,
+      name: "Chichen Itza (Yucatan Peninsula, Mexico)",
+      link: "https://www.youtube.com/embed/0PqTqcGoS0w?start=0"
+
+    },
+    {
+      id: 6,
+      name: "The Roman Colosseum (Rome)",
+      link: "https://www.youtube.com/embed/1j2keKi-gcs?start=136"
+
+    },
+    {
+      id: 7,
+      name: "Petra (Jordan)",
+      link: "https://www.youtube.com/embed/HCoyRrylVrg?start=7"
+
+    },
+    {
+      id: 8,
+      name: "Burj Khalifa",
+      link: "https://www.youtube.com/embed/po4Y7mX8P2A?start=0"
+
+    },
+    {
+      id: 9,
+      name: "Bora Bora Iceland French Polynesia",
+      link: "https://www.youtube.com/embed/O_9TgmIg1nA?start=6"
+
+    },
+    {
+      id: 10,
+      name: "Costa Rica",
+      link: "https://www.youtube.com/embed/LXb3EKWsInQ?start=0&end=303"
+
+    },
+    {
+      id: 11,
+      name: "Pyramids of Giza, Egypt",
+      link: "https://www.youtube.com/embed/Dtw2vfKihXA?start=10"
+
+    }
+  ];
+
+  export default LIST;
